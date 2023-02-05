6406d3375eba79bf1bf496a4d50a5396d4182d75
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-02-05 23:37:22 +0800

    e

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,5 @@
 	import "../app.css";
 </script>
 
-<div class="absolute inset-y-0 left-0 z-20 bg-black/[.6] w-full h-16">
-	<a href="/" class="text-white">
-		<img src="/favicon.png" alt="Home" class="h-14 pt-2 pl-3">
-	</a>
-</div>
 
 <slot />
