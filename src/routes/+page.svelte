ca7eb220abfdcb18890ace85aa97660ca3d747bd
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-02-05 23:07:08 +0800

    E

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,21 @@
 	import Typewriter from 'svelte-typewriter'
 	let previousTime = Date.now();
 </script>
-<div class="h-2/3">
+<div class="">
 	<img src="{heading}" alt="Raffles Voices Group" class="gradient-mask-b-0">
 </div>
 
 
 <div>
-	<h1 class="text-4xl md:text-9xl pb-10 pl-14 text-white font-pt-serif font-bold">Raffles Voices</h1>
+	<h1 class="text-4xl md:text-9xl pb-5 pl-14 text-white font-pt-serif font-bold">Raffles Voices</h1>
 	<Typewriter mode='loop'>
 		<p class="text-2xl md:text-3xl pl-14 text-white font-pt-serif font-bold">A choir of excellence</p>
 		<p class="text-2xl md:text-3xl pl-14 text-white font-pt-serif font-bold">A choral community</p>
 		<p class="text-2xl md:text-3xl pl-14 text-white font-pt-serif font-bold">Not a community choir</p>
 	</Typewriter>
 </div>
-<div class="w-9/12">
-	<h2 class="text-3xl md:text-5xl pt-10 md:pt-32 pb-8 pl-14 text-blue-400 font-canela-serif italic">Who are we?</h2>
+<div class="w-11/12">
+	<h2 class="text-3xl md:text-5xl pt-10 md:pt-32 pb-4 pl-14 text-blue-400 font-canela-serif italic">Who are we?</h2>
 	<p class="text-xl md:text-2xl pl-14 text-white font-canela-serif">Raffles Voices, the choir of Raffles Institution, was founded in 1992 by Ms Charmaine Chiang.  Under the directorship of award-winning conductor Mr. Toh Ban Sheng since 2003, it has grown into a renowned SATB boys choir, garnering numerous top accolades in Europe and tackling a myriad of musical genres from classical works to modern commissions.</p>
 	<p class="text-xl md:text-2xl pl-14 pt-5 text-white font-canela-serif">Raffles Voices is active in the choral arena, participating in a wide range of choral competitions from the Singapore Youth Festival (SYF) Central Judging to international choral festivals. Besides consistently achieving Gold (with Honours) and Distinction Certificate in SYF, Raffles Voices has established itself as one of the foremost choirs in Singapore since clinching its first Gold in Germany in 2006.  Perhaps the most awarded secondary school choir, at the heel of winning two Gold Awards in Vienna, a Category Champion and the Conductor's Prize in 2010, it was ranked 11th of the top 50 children's choirs by Interkultur World Ranking.</p>
 </div>
