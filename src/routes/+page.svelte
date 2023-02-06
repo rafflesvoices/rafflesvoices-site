89497cca32e40e5fb85e7edbdbf451b85e2d43d0
Sherlock Holmes <sherlockholmesxp@gmail.com>
2023-02-06 12:06:22 +0800

    achievements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,3 +21,6 @@
 	<p class="text-xl md:text-2xl pl-14 text-white font-canela-serif">Raffles Voices, the choir of Raffles Institution, was founded in 1992 by Ms Charmaine Chiang.  Under the directorship of award-winning conductor Mr. Toh Ban Sheng since 2003, it has grown into a renowned SATB boys choir, garnering numerous top accolades in Europe and tackling a myriad of musical genres from classical works to modern commissions.</p>
 	<p class="text-xl md:text-2xl pl-14 pt-5 text-white font-canela-serif">Raffles Voices is active in the choral arena, participating in a wide range of choral competitions from the Singapore Youth Festival (SYF) Central Judging to international choral festivals. Besides consistently achieving Gold (with Honours) and Distinction Certificate in SYF, Raffles Voices has established itself as one of the foremost choirs in Singapore since clinching its first Gold in Germany in 2006.  Perhaps the most awarded secondary school choir, at the heel of winning two Gold Awards in Vienna, a Category Champion and the Conductor's Prize in 2010, it was ranked 11th of the top 50 children's choirs by Interkultur World Ranking.</p>
 </div>
+<div class="w-11/12">
+	<h2 class="text-3xl md:text-5xl pt-10 md:pt-32 pb-4 pl-14 text-blue-400 font-canela-serif italic">Our Past Achievements</h2>
+</div>
